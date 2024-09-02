da162899534e7814790c46f783574be9b85fa035
Jat-in-cmd <chandnanijatin16@gmail.com>
2024-09-02 10:19:03 +0530

    Add files via upload

---
+++ 2calcy.py
@@ -0,0 +1,11 @@
+op=str(input("enter the operator :"))
+on1=int(input("enter the operand 1:"))
+on2=int(input("enter the operand 2:"))
+if(op=='*'):
+    print(on1*on2)
+if(op=='/'):
+    print(on1/on2)
+if(op=='+'):
+    print(on1+on2)
+if(op=='-'):
+    print(on1-on2)
