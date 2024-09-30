179f6e6a8483e1e6b919e02634ba7760c1e54307
Jat-in-cmd <chandnanijatin16@gmail.com>
2024-09-30 10:33:40 +0530

    Delete 3reverseNum.py

--- 3reverseNum.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter a number :"))
-Rnm=0
-while n!=0 :
-    y=n%10
-    n=int(n/10)
-    Rnm=Rnm*10+y
-print(Rnm)
-if(Rnm==n): print("it is a palindrome")
-if(Rnm!=n): print("it is not a palindrome")
